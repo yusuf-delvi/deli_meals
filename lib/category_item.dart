690bfebb5dfb723b65b991094576b9c4e89346ef
Yusuf Mohammed Delvi <yusufmddelvi8@gmail.com>
2022-12-20 21:39:54 +0530

    navigate to meals page on tap on category

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -1,29 +1,46 @@
 import 'package:flutter/material.dart';
 
+import './category_meals_page.dart';
+
 class CategoryItem extends StatelessWidget {
   const CategoryItem(this.title, this.color, {super.key});
 
   final String title;
   final Color color;
 
+  void selectCategory(ctx) {
+    Navigator.of(ctx).push(
+      MaterialPageRoute(
+        builder: (_) {
+          return const CategoryMealsPage();
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15),
-        gradient: LinearGradient(
-          colors: [
-            color.withOpacity(0.7),
-            color,
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+    return InkWell(
+      onTap: () => selectCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(10),
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          gradient: LinearGradient(
+            colors: [
+              color.withOpacity(0.7),
+              color,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
         ),
-      ),
-      child: Text(
-        title,
-        style: Theme.of(context).textTheme.headline6,
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.headline6,
+        ),
       ),
     );
   }
