60fe4f101c34b9975a3d3bedecd4f8163d1c1b29
Yusuf Mohammed Delvi <yusufmddelvi8@gmail.com>
2022-12-21 22:55:03 +0530

    move categories scafold to respective page

--- lib/pages/categories_page.dart
+++ lib/pages/categories_page.dart
@@ -8,20 +8,28 @@ class CategoriesPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('DeliMeals'),
       ),
-      padding: const EdgeInsets.all(20),
-      children: <Widget>[
-        ...(DUMMY_CATEGORIES
-            .map((catData) =>
-                CategoryItem(catData.id, catData.title, catData.color))
-            .toList())
-      ],
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        padding: const EdgeInsets.all(20),
+        children: <Widget>[
+          ...(DUMMY_CATEGORIES
+              .map((catData) => CategoryItem(
+                    catData.id,
+                    catData.title,
+                    catData.color,
+                  ))
+              .toList())
+        ],
+      ),
     );
   }
 }
