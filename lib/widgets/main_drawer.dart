c1ea2d52838ca49f9dcc049ac4cf9c8bf0d45afc
Yusuf Mohammed Delvi <yusufmddelvi8@gmail.com>
2022-12-22 20:45:22 +0530

    wrap main drawer with safearea

--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -23,28 +23,30 @@ class MainDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      child: Column(
-        children: [
-          Container(
-            height: 120,
-            width: double.infinity,
-            padding: const EdgeInsets.all(20),
-            alignment: Alignment.centerLeft,
-            color: Theme.of(context).canvasColor,
-            child: Text(
-              'Cooking Up!',
-              style: TextStyle(
-                fontWeight: FontWeight.w900,
-                fontSize: 30,
-                color: Theme.of(context).colorScheme.primary,
+    return SafeArea(
+      bottom: false,
+      child: Drawer(
+        child: Column(
+          children: [
+            Container(
+              height: 120,
+              width: double.infinity,
+              padding: const EdgeInsets.all(20),
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Cooking Up!',
+                style: TextStyle(
+                  fontWeight: FontWeight.w900,
+                  fontSize: 30,
+                  color: Theme.of(context).colorScheme.primary,
+                ),
               ),
             ),
-          ),
-          const SizedBox(height: 20),
-          LinkRow('Meals', Icons.restaurant),
-          LinkRow('Filters', Icons.settings),
-        ],
+            const SizedBox(height: 20),
+            LinkRow('Meals', Icons.restaurant),
+            LinkRow('Filters', Icons.settings),
+          ],
+        ),
       ),
     );
   }
