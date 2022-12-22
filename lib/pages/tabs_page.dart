c2cf338e9f84ea86eeea724c32216355c0db879f
Yusuf Mohammed Delvi <yusufmddelvi8@gmail.com>
2022-12-22 19:11:22 +0530

    add tabs page widget

---
+++ lib/pages/tabs_page.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+import './categories_page.dart';
+import './favorites_page.dart';
+
+class TabsPage extends StatelessWidget {
+  const TabsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      initialIndex: 0,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('DeliMeals'),
+          bottom: const TabBar(
+            tabs: [
+              Tab(
+                icon: Icon(Icons.category),
+                text: 'Categories',
+              ),
+              Tab(
+                icon: Icon(Icons.star),
+                text: 'Favorites',
+              ),
+            ],
+          ),
+        ),
+        body: const TabBarView(
+          children: [
+            CategoriesPage(),
+            FavoritesPage(),
+          ],
+        ),
+      ),
+    );
+  }
+}
